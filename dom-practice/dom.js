30682d1c5b0c4422093c37de9e755b6f4494c743
devVGBG <resourcefulmindoso@gmail.com>
2022-03-11 15:05:59 +0100

    --- Auto-Git Commit ---

--- dom-practice/dom.js
+++ dom-practice/dom.js
@@ -1,11 +1,14 @@
 //to type an item and be able to submit
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 //to Add Items
 form.addEventListener('submit', addItem);
 //to remove items
 itemList.addEventListener('click', removeItem)
+//to filter items
+filter.addEventListener('keyup', filterItems);
 
 //function for adding item
 function addItem(e) {
@@ -43,4 +46,24 @@ function removeItem(e) {
             itemList.removeChild(li);
         }
     }
+}
+
+function filterItems(e) {
+    //first extract the value of what is entered into the search bar and convert everything to lowercase
+    var filterText = e.target.value.toLowerCase();
+    console.log(filterText);
+    //target all the items on the list and store them in a variable for array methods
+    var items = itemList.getElementsByTagName('li');
+    //convert the html collection into an array
+    Array.from(items).forEach(function(item) {
+        var itemName = item.firstChild.textContent;
+        //compare item to what's in the searchbox text
+        if(itemName.toLowerCase().indexOf(filterText) != -1){
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+            
+        }
+    })
+
 }
