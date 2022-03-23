7c2482f3d575d98d92895c9d93dc6c8e51775933
devVGBG <resourcefulmindoso@gmail.com>
2022-03-23 14:00:58 +0100

    add new reusable component CommentDetail and add props author, timesTamp and comment

---
+++ learning-react-js/comments-section-app/src/CommentDetail.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const CommentDetail = (props) => {
+    return (
+        <div className="comment">
+                <a href="/" className='avatar'>
+                    <img alt="avatar" src={props.image} />
+                </a>
+                <div className="content">
+                    <a href="/" className='author'>{props.author}</a>
+                    <div className="metadata">
+                        <span className="date">{props.timeStamp}</span>
+                    </div>
+                    <div className="text">{props.comment}</div>
+                </div>
+            </div>
+    );
+};
+
+export default CommentDetail;
--- learning-react-js/comments-section-app/src/index.js
+++ learning-react-js/comments-section-app/src/index.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
 
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className='avatar'>
-                    <img alt="avatar" src={"https://via.placeholder.com/150"} />
-                </a>
-                <div className="content">
-                    <a href="/" className='author'>Nader Dabit</a>
-                    <div className="metadata">
-                        <span className="date">3/18/22 at 4:30PM</span>
-                    </div>
-                    <div className="text">I remember when I was first learning Git, the phrase "pull request" made no sense to me and confused me so much, now I am 100% comfortable using Git but still think that phrase makes no sense at all.</div>
-                </div>
-            </div>
+            <CommentDetail author="Nader Dabit" timeStamp="3/18/22 at 4:30PM" comment="I remember when I was first learning Git, the phrase 'pull request' made no sense to me and confused me so much, now I am 100% comfortable using Git but still think that phrase makes no sense at all." image="https://via.placeholder.com/150"/>
+            <CommentDetail author="Bianca Gandolfo" timeStamp="2/1/22 at 6:09PM" comment="Wanted to shoutout @1Marc and the @FrontendMasters team for always believing in me even when I didn't believe in myself. Do you have someone who has believed in you and changed your life? Let them know." image="https://via.placeholder.com/150"/>
+            <CommentDetail author="Guillaume Bibeau" timeStamp="3/19/22 at 3:39AM" comment="All my life I've worked at half my capacity. Feeling unchallenged and bored by almost everything. Now I can't stop opening my work @vercel I just have so much fun pushing myself to make the web fast. It's like a drug." image="https://via.placeholder.com/150"/>
         </div>
     )
 };
