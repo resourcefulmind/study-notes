dbbc08c27146e9216f9b6840acd488ebf34a7c1f
devVGBG <resourcefulmindoso@gmail.com>
2022-03-30 13:07:14 +0100

    add conditional render, refactor of state and implementation of lifecycle method

--- learning-react-js/seasons-app/src/index.js
+++ learning-react-js/seasons-app/src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position), 
-        (err) => console.log(err)
-    );
-    return <div>Let us begin</div>
-};
+class App extends React.Component {
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                //this should be the normal way to set your state.
+                this.setState({ lat: position.coords.latitude });
+            }, 
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
+    }
+
+    //if you do not define render, React will throw an error. It is non-negotiable
+    render() {
+            if (this.state.errorMessage && !this.state.lat) {
+                return <div>Error: { this.state.errorMessage }</div>;
+            }
+
+            if (!this.state.errorMessage && this.state.lat) {
+                return <div>Latitude: { this.state.lat }</div>;
+            }
+
+            return <div>Loading...</div>;
+    };
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
