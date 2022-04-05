2ebbefc7412df0f6ff95df3a34e5650281375a52
devVGBG <resourcefulmindoso@gmail.com>
2022-04-05 14:21:06 +0100

    add functionality to app, pass state by props

--- learning-react-js/seasons-app/src/SeasonDisplay.js
+++ learning-react-js/seasons-app/src/SeasonDisplay.js
@@ -1,6 +1,14 @@
 import React from "react";
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 5) {
+        return lat > 0 ? 'It is Spring...' : 'It\'s the Rainy Season, make sure you take your umbrella and rain coats with you. Be Careful while driving too';
+    } else if (month >)
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+
     return <div>Season Display</div>
 };
 
--- learning-react-js/seasons-app/src/index.js
+++ learning-react-js/seasons-app/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
     state = { lat: null, errorMessage: '' };
@@ -23,7 +24,7 @@ class App extends React.Component {
             }
 
             if (!this.state.errorMessage && this.state.lat) {
-                return <div>Latitude: { this.state.lat }</div>;
+                return <SeasonDisplay lat = {this.state.lat} />
             }
 
             return <div>Loading...</div>;
