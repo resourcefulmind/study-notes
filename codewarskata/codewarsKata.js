c642fbdeb3eada721ed4c1f1bba1e52161f320fb
devVGBG <resourcefulmindoso@gmail.com>
2022-04-25 10:05:16 +0100

    add challenge for 25th April, 2022

--- codewarskata/codewarsKata.js
+++ codewarskata/codewarsKata.js
@@ -114,3 +114,62 @@ var summation = function (num) {
   }
   return result;
 }
+
+// Kata for 20th April, 2Convert number to reversed array of digits
+
+//first task
+//Given a random non-negative number, you have to return the digits of this number within an array in reverse order
+//my thought process here would be to push that number into a new array, split up the array, reverse it and then join it all back together again
+function digitize(n) {
+  //first use the split method to split the number and put them into an array
+  var arr = (""+n).split('');
+  //declare an empty array which the reversed numbers are going to be passed into
+  var reversedArr = [];
+  //reverse the array with a for loop that starts at the index of the arr length less than 1 and decreases by 1 and use a parse int to change the stringlike back numbers into real numbers
+  for (var i = arr.length - 1; i >= 0; i--) {
+  reversedArr[i] = parseInt(arr.shift(), 10);
+	}
+	return reversedArr;
+}
+
+console.log(digitize(32245));
+console.log(digitize(455646636337373));
+
+//second task
+// In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+
+// At the end of the first year there will be: 
+// 1000 + 1000 * 0.02 + 50 => 1070 inhabitants
+
+// At the end of the 2nd year there will be: 
+// 1070 + 1070 * 0.02 + 50 => 1141 inhabitants (** number of inhabitants is an integer **)
+
+// At the end of the 3rd year there will be:
+// 1141 + 1141 * 0.02 + 50 => 1213
+
+// It will need 3 entire years.
+// More generally given parameters:
+
+// p0, percent, aug (inhabitants coming or leaving each year), p (population to surpass)
+
+// the function nb_year should return n number of entire years needed to get a population greater or equal to p.
+
+// aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
+
+// Examples:
+// nb_year(1500, 5, 100, 5000) -> 15
+// nb_year(1500000, 2.5, 10000, 2000000) -> 10
+// Note:
+// Don't forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.
+
+// Solution an be achieved using a for loop and rounding down for each year
+function nbYear(p0, percent, aug, p) {
+    
+  for (var years = 0; p0 < p; years++) {
+    p0 = Math.floor(p0 + p0 * percent / 100 + aug);
+  }
+  return years
+}
+
+nb_year(1500, 5, 100, 5000) -> 15
+nb_year(1500000, 2.5, 10000, 2000000) -> 10
