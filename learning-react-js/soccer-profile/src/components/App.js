77e2a9b49ce2b785149946c37d43b30e721cfdb4
resourcefulmind <resourcefulmindoso@gmail.com>
2022-05-03 12:36:25 +0100

    chore: implement API GET request functionality, params, root url and headers

--- learning-react-js/soccer-profile/src/components/App.js
+++ learning-react-js/soccer-profile/src/components/App.js
@@ -5,8 +5,13 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
-    
+    axios.get('https://api-football-v1.p.rapidapi.com/v3/players', {
+      params: { search: term }, 
+      headers: {
+        'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com',
+        'X-RapidAPI-Key': '0f2fba8377msh8b3ae4d192f719fp1f7a3djsn189ee6ead54a'
+      }
+    }) 
   }
 
   render() {
--- learning-react-js/soccer-profile/src/components/SearchBar.js
+++ learning-react-js/soccer-profile/src/components/SearchBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    state = { term: '' };
+    state = { term: '', secondTerm: '' };
 
     onFormSubmit = (event) => {
         event.preventDefault();
 
+        // this.props.onSubmit(this.state.secondTerm);
         this.props.onSubmit(this.state.term);
     }
 
@@ -15,6 +16,8 @@ class SearchBar extends React.Component {
                 <h1>Opeyemi's Soccer Profile App</h1>
                 <form onSubmit={this.onFormSubmit} className='ui form'>
                     <div className="field">
+                        {/* <label>Enter a league(Has to be a number)</label>
+                        <input type="text" value={this.state.secondTerm} onChange={e => this.setState({ secondTerm: e.target.value })} /> */}
                         <label>Enter A Player Name</label>
                         <input type="text" value={this.state.term} onChange={e => this.setState({ term: e.target.value })} />
                     </div>
