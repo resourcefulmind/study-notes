83e22e49d22d2fff6e82fda4258883a212ec724d
devVGBG <resourcefulmindoso@gmail.com>
2022-05-03 09:25:44 +0100

    chore: add form and event handlers

--- learning-react-js/soccer-profile/src/components/App.js
+++ learning-react-js/soccer-profile/src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 
 class App extends React.Component {
   onSearchSubmit(term) {
     console.log(term);
+    
   }
 
   render() {
