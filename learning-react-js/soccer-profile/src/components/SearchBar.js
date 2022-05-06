c19a1dc12e297d640ee0af5f2fd1228eaaa3a7b4
resourcefulmind <resourcefulmindoso@gmail.com>
2022-05-06 12:11:55 +0100

    chore: change label to coaches and focus app on coaches as they do not get as much popularity as the players

--- learning-react-js/soccer-profile/src/components/SearchBar.js
+++ learning-react-js/soccer-profile/src/components/SearchBar.js
@@ -1,24 +1,21 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    state = { term: '', secondTerm: '' };
+    state = { term: '' };
 
     onFormSubmit = (event) => {
         event.preventDefault();
 
-        // this.props.onSubmit(this.state.secondTerm);
         this.props.onSubmit(this.state.term);
     }
 
     render() {
         return (
             <div className="ui segment">
-                <h1>Opeyemi's Soccer Profile App</h1>
+                <h1>Opeyemi's Soccer Coaches Profile App</h1>
                 <form onSubmit={this.onFormSubmit} className='ui form'>
                     <div className="field">
-                        {/* <label>Enter a league(Has to be a number)</label>
-                        <input type="text" value={this.state.secondTerm} onChange={e => this.setState({ secondTerm: e.target.value })} /> */}
-                        <label>Enter A Player Name</label>
+                        <label>Enter The Name Of A Soccer Coach e.g Klopp or Guardiola</label>
                         <input type="text" value={this.state.term} onChange={e => this.setState({ term: e.target.value })} />
                     </div>
                 </form>
@@ -26,5 +23,6 @@ class SearchBar extends React.Component {
         );
     }
 }
+
 
 export default SearchBar;
