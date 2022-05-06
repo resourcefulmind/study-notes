a96041621268fcd65ca5a7b5c7dc0b1fcdb6b7fd
resourcefulmind <resourcefulmindoso@gmail.com>
2022-05-06 12:08:57 +0100

    chore: add rapidapi custom client, add state and coplete network request with params

--- learning-react-js/soccer-profile/src/components/App.js
+++ learning-react-js/soccer-profile/src/components/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import axios from 'axios';
+import rapidapi from '../api/rapidapi';
 import SearchBar from './SearchBar';
 
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api-football-v1.p.rapidapi.com/v3/players', {
-      params: { search: term }, 
-      headers: {
-        'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com',
-        'X-RapidAPI-Key': '0f2fba8377msh8b3ae4d192f719fp1f7a3djsn189ee6ead54a'
-      }
-    }) 
+  state = { profile: [] }
+
+  onSearchSubmit = async (term) => {
+    const response = await rapidapi.get('/v3/coachs', {
+      params: { search: term }
+    });
+    // .then(response => {
+    //   console.log(response.data.response);
+    // }); this is the .then() method
+    this.setState({ profile: response.data.response });
   }
 
   render() {
     return (
       <div className='ui container' style={{ marginTop: '10px' }}>
           <SearchBar onSubmit={this.onSearchSubmit} />
+          This search returned {this.state.profile.length} profile(s).
       </div>
     );
   }
