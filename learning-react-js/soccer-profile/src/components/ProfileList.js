c67b05639b1a3a87ca93818d045677765e9fdb69
resourcefulmind <resourcefulmindoso@gmail.com>
2022-05-09 16:12:33 +0100

    chore: display coach profile

--- learning-react-js/soccer-profile/src/components/ProfileList.js
+++ learning-react-js/soccer-profile/src/components/ProfileList.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
 const ProfileList = (props) => {
+    console.log(props.profiles)
+      const profiles = props.profiles.map((profile) => {
+         return (
+                <div> 
+                    {profile.career} 
+                </div>
+         );
+      });
 
-
-    return <div>ProfileList</div>
+     return (
+            <div>
+                {profiles}
+            </div>
+     );
 };
 
 export default ProfileList;
