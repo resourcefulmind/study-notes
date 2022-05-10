cd12202c1c692aeb71942d01a7ab9c866a0d4bb6
resourcefulmind <resourcefulmindoso@gmail.com>
2022-05-10 04:35:53 +0100

    chore: implement coach profile list building functionality

--- learning-react-js/soccer-profile/src/components/ProfileList.js
+++ learning-react-js/soccer-profile/src/components/ProfileList.js
@@ -4,8 +4,45 @@ const ProfileList = (props) => {
     console.log(props.profiles)
       const profiles = props.profiles.map((profile) => {
          return (
+                
                 <div> 
-                    {profile.career} 
+                    <div>
+                    <h1>{profile.firstname} {profile.lastname}</h1>
+                    <img src={profile.photo} alt='Jurgen Klopp'/>
+                    <br />
+                    <br />
+                    <p>Date of Birth: {profile.birth.date}</p>
+                    <p>Nationality: {profile.nationality}</p> 
+                    <p>Birthplace: {profile.birth.place}</p>
+                    <p>Height: {profile.height}</p>
+                    <p>Weight: {profile.weight}</p>
+                    <p>Current Team: {profile.team.name}</p>
+                    <img src={profile.team.logo} alt='Liverpool FC' />
+                    </div>
+                    <br />
+                    <div>
+                        <h2>Career</h2>
+                        <p>{profile.firstname} {profile.lastname}, since starting his managerial career has coached the following teams: </p>
+                        <div>
+                            <h1>{profile.career[0].team.name}</h1>
+                            <img src={profile.career[0].team.logo} />
+                            <p>Started Role As Head Coach: {profile.career[0].start}</p>
+                            <p>Ended Role As Head Coach: {profile.career[0].end}</p>
+                        </div>
+                        <div>
+                            <h1>{profile.career[1].team.name}</h1>
+                            <img src={profile.career[1].team.logo} />
+                            <p>Started Role As Head Coach: {profile.career[1].start}</p>
+                            <p>Ended Role As Head Coach: {profile.career[1].end}</p>
+                        </div>
+                        <div>
+                            <h1>{profile.career[2].team.name}</h1>
+                            <img src={profile.career[2].team.logo} />
+                            <p>Started Role As Head Coach: {profile.career[2].start}</p>
+                            <p>Ended Role As Head Coach: {profile.career[2].end}</p>
+                        </div>
+                        
+                    </div>
                 </div>
          );
       });
