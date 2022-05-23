71d74def40635d24fa4bdad29ee477efcfb0a3b0
resourcefulmind <resourcefulmindoso@gmail.com>
2022-05-23 12:16:50 +0100

    chore: add more structure to app to enable easier styling

--- learning-react-js/soccer-profile/src/components/ProfileList.js
+++ learning-react-js/soccer-profile/src/components/ProfileList.js
@@ -1,3 +1,4 @@
+import './ProfileList.css'
 import React from 'react';
 
 const ProfileList = (props) => {
@@ -5,50 +6,67 @@ const ProfileList = (props) => {
       const profiles = props.profiles.map((profile) => {
          return (
                 
-                <div key={profile.id}> 
-                    <div>
+                <div className='container' key={profile.id}>
                     <h1>{profile.firstname} {profile.lastname}</h1>
-                    <img src={profile.photo} alt='Jurgen Klopp'/>
-                    <br />
-                    <br />
-                    <p>Date of Birth: {profile.birth.date}</p>
-                    <p>Nationality: {profile.nationality}</p> 
-                    <p>Birthplace: {profile.birth.place}</p>
-                    <p>Height: {profile.height}</p>
-                    <p>Weight: {profile.weight}</p>
-                    <p>Current Team: {profile.team.name}</p>
-                    <img src={profile.team.logo} alt='Liverpool FC' />
+                    <div className='section'>
+                            <div className='first-sub'>
+                                <img src={profile.photo} alt='Jurgen Klopp'/>
+                                <br />
+                                <br />
+                            </div>
+                            <div className='second-sub'>
+                                <p><span>Date of Birth:</span> {profile.birth.date}</p>
+                                <p><span>Nationality:</span> {profile.nationality}</p> 
+                                <p><span>Birthplace:</span> {profile.birth.place}</p>
+                                <p><span>Height:</span> {profile.height}</p>
+                                <p><span>Weight:</span> {profile.weight}</p>
+                            </div>
+                            <div className='third-sub'>
+                                <p><span>Current Team:</span> {profile.team.name}</p>
+                                <img src={profile.team.logo} alt='Liverpool FC' />
+                            </div>
                     </div>
                     <br />
+                    <h2>Career</h2>
+                    <p>{profile.firstname} {profile.lastname}, since starting his managerial career has coached the following teams: </p>
                     <div>
-                        <h2>Career</h2>
-                        <p>{profile.firstname} {profile.lastname}, since starting his managerial career has coached the following teams: </p>
-                        <div>
-                            <h1>{profile.career[0].team.name}</h1>
-                            <img src={profile.career[0].team.logo} alt={profile.career[0].team.name}/>
-                            <p>Started Role As Head Coach: {profile.career[0].start}</p>
-                            <p>Ended Role As Head Coach: {profile.career[0].end}</p>
+                        <div className='clubs-section'>
+                            <div>
+                                <h1>{profile.career[0].team.name}</h1>
+                                <img src={profile.career[0].team.logo} alt={profile.career[0].team.name}/>
+                            </div>
+                            <div>
+                                <p>Started Role As Head Coach: {profile.career[0].start}</p>
+                                <p>Ended Role As Head Coach: {profile.career[0].end}</p>
+                            </div>
                         </div>
-                        <div>
-                            <h1>{profile.career[1].team.name}</h1>
-                            <img src={profile.career[1].team.logo} alt={profile.career[1].team.name}/>
-                            <p>Started Role As Head Coach: {profile.career[1].start}</p>
-                            <p>Ended Role As Head Coach: {profile.career[1].end}</p>
+                        <div className='clubs-section'>    
+                            <div>
+                                <h1>{profile.career[1].team.name}</h1>
+                                <img src={profile.career[1].team.logo} alt={profile.career[1].team.name}/>
+                            </div>
+                            <div>
+                                <p>Started Role As Head Coach: {profile.career[1].start}</p>
+                                <p>Ended Role As Head Coach: {profile.career[1].end}</p>
+                            </div>
                         </div>
-                        <div>
-                            <h1>{profile.career[2].team.name}</h1>
-                            <img src={profile.career[2].team.logo} alt={profile.career[2].team.name}/>
-                            <p>Started Role As Head Coach: {profile.career[2].start}</p>
-                            <p>Ended Role As Head Coach: {profile.career[2].end}</p>
+                        <div className='clubs-section bottom'>
+                            <div>
+                                <h1>{profile.career[2].team.name}</h1>
+                                <img src={profile.career[2].team.logo} alt={profile.career[2].team.name}/>
+                            </div>
+                            <div>
+                                <p>Started Role As Head Coach: {profile.career[2].start}</p>
+                                <p>Ended Role As Head Coach: {profile.career[2].end}</p>
+                            </div>
                         </div>
-                        
                     </div>
                 </div>
          );
       });
 
      return (
-            <div>
+            <div className='profile-list'>
                 {profiles}
             </div>
      );
