97c0e2d0782f85f1a5bf5a5128b4e402abcc1206
resourcefulmind <resourcefulmindoso@gmail.com>
2022-07-04 07:51:44 +0100

    task: my daily codewars kata solution. 4th July

--- codewarskata/codewarsKata.js
+++ codewarskata/codewarsKata.js
@@ -428,4 +428,100 @@ function century(year) {
 1601 --> 17
 2000 --> 20
 
+// Kata for 4th July,2022 
+// First Task
 
+// You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+// Write a program that returns the girl's age (0-9) as an integer.
+
+// Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+
+//Solution and Thought Process
+
+//There are some things to think about here
+
+//An input string is being passed in and the problem says it always starts with a number. Function is also supposed to return an integer which means I have to check how to return integers from strings.
+//Solution can either be gotten by using the parseInt() method on the parameter the function is taking in which is inputString or making sure the second index of the the string is strictly numerical using the Number() method.
+
+//I prefer the parseInt() method so solution goes thus
+
+function getAge(inputString){
+  // return the girl's correct age as an integer. Happy coding :) 
+    return parseInt(inputString);
+}
+
+
+// Second Task
+// Grade book
+
+// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+
+//Thought Process/Solution
+
+// This is pretty straightforward. A variable needs to be declared to store the sum of the scores and take an average. My spaghetti code solution below
+function getGrade (s1, s2, s3) {
+  // Code here
+  let sum = s1 + s2 + s3;
+  let score = sum / 3;
+  if (score >= 90 && score <= 100) {
+    return 'A'
+  } 
+  if (score >= 80 && score < 90) {
+    return 'B'
+  }
+  if (score >= 70 && score < 80) {
+    return 'C'
+  }
+  if (score >= 60 && score < 70) {
+    return 'D'
+  }
+  if (score >= 0 && score < 60) {
+    return 'F'
+  }
+}
+
+//Another solution I discovered and really liked is
+function getGrade (s1, s2, s3) {
+  var avg = (s1 + s2 + s3)/3;
+  switch(true) {
+    case (avg >= 90):
+      return 'A';
+    case ( avg >= 80):
+      return 'B';
+    case ( avg >= 70):
+      return 'C';
+    case ( avg >= 60):
+      return 'D'; 
+    default:
+      return 'F';   
+  }
+}
+
+// Third Task
+// The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+// For example:
+
+// 1.08 --> 30
+// Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
+
+function cockroachSpeed(s) {
+  //Good Luck!
+  //speed will always be distance over time
+  //convert distance from km to cm
+  //convert time from hour to second
+  //round down the result
+  let distanceConversion = 100000;
+  let timeConversion = 3600;
+  
+  return Math.floor(s * (distanceConversion / timeConversion))
+}
